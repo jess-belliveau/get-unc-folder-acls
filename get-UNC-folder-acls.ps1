9372dac4053ac55b93c176b4fc712aaedfd05b0a
Jess Belliveau <jess.belliveau@gmail.com>
2012-06-26 16:38:58 +1000

    changed domain to be variable
    makes domain agnostic

--- get-UNC-folder-acls.ps1
+++ get-UNC-folder-acls.ps1
@@ -1,24 +1,26 @@
 ﻿<#
 .SYNOPSIS
-Gets and counts members of a group caught as a unique ACL containing ASIAPAC in its name
+Gets and counts members of a group caught as a unique ACL containing <domain> in its name
 .PARAMETER
-The UNC path of a folder
+The UNC path of a folder and a target
 .DESCRIPTION
-Determines unique ACL groups that contain ASIAPAC in their name from a user provided UNC path
+Determines unique ACL groups that contain <domain> in their name from a user provided UNC path
 .EXAMPLE
-get-UNC-folder-acls.ps1 -UNCPath \\cscsrvmel7\d$\Groups -target_folder VWA
+get-UNC-folder-acls.ps1 -UNCPath \\<server name>\d$\Groups -target_folder VWA
 #>
 [CmdletBinding()]
 param (
 	[Parameter(Mandatory=$true)]
 	[string]$UNCPath,
 	[Parameter(Mandatory=$true)]
-	[string]$target_folder
+	[string]$target_folder,
+	[Parameter(Mandatory=$true)]
+	[string]$domain
 )
 
 $previous_path=pwd
 cd $UNCPath
-$groups = get-acl $target_folder | select -expand access | where-object {$_.identityreference -like "*ASIAPAC*"} | select-Object -unique -expand identityreference 
+$groups = get-acl $target_folder | select -expand access | where-object {$_.identityreference -like "*$domain*"} | select-Object -unique -expand identityreference 
 foreach ($group in $groups) {
 	get-qadgroupmember $group.ToString() | measure-object
 }
